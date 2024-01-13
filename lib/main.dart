fbac31bbc300388bce2b0fdb8244406f99fde6e8
bhagyanimash <bhagyanimash2000@gmail.com>
2024-01-13 11:21:55 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BMIcalculator());
+}
+
+class BMIcalculator extends StatelessWidget {
+  const BMIcalculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
