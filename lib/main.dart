f866b61700b21cbd2b4b4cce418f790510010124
bhagyanimash <bhagyanimash2000@gmail.com>
2024-01-15 20:18:37 +0530

    Creating theme

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,39 @@ class BMIcalculator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+        //brightness: Brightness.dark,
+        //scaffoldBackgroundColor: Colors.tealAccent[100]
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const InputPage(),
+    );
+  }
+}
+
+class InputPage extends StatefulWidget {
+  const InputPage({super.key});
+
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BMI Calculator'),
+      ),
+      body: const Center(
+        child: Text('Body Text'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
